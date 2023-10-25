07faa121a8178890ff8817f5307d2c1727a9f54c
Agahuseyn-Agayev <agahuseynagayev019@gmail.com>
2023-10-25 14:31:23 +0400

    Added ms-ingress-comment

---
+++ src/main/java/com/example/msingresscomment/MsIngressCommentApplication.java
@@ -0,0 +1,13 @@
+package com.example.msingresscomment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsIngressCommentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsIngressCommentApplication.class, args);
+	}
+
+}
---
+++ src/test/groovy/com/example/msingresscomment/MsIngressCommentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msingresscomment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsIngressCommentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
